68edda0488d7639d08b0642a134a8bf30081c995
emilyytsai <emilytsai@csu.fullerton.edu>
2024-10-05 02:25:42 -0700

    Fixed the fish spawner and added fish spin out animations

--- Assets/FishSpawner.cs
+++ Assets/FishSpawner.cs
@@ -8,8 +8,8 @@ public class FishSpawner : MonoBehaviour
     private GameObject fishPrefab;
 
     [SerializeField]
-    //spawns a new fish every 3.5 seconds
-    private float fishInterval = 3.5f;
+    //spawns a new fish every 3 seconds
+    private float fishInterval = 3f;
 
     void Start()
     {
@@ -22,8 +22,8 @@ public class FishSpawner : MonoBehaviour
         while (true)
         {
             yield return new WaitForSeconds(interval);
-            //creates the new fish at random values/positions using the vector
-            GameObject newFish = Instantiate(fish, new Vector3(Random.Range(-5f, 5), Random.Range(-6f, 6f), 0), Quaternion.identity);
+            //creates the new fish at the same point
+            GameObject newFish = Instantiate(fish, new Vector3(0, 0, 0), Quaternion.identity);
         }
     }
 }
--- Assets/fishController.cs
+++ Assets/fishController.cs
@@ -11,15 +11,32 @@ public class fishController : MonoBehaviour
     [SerializeField]
     Vector2 fishSpawnLocation;
 
+    //list for the fish animations which we'll access thru the trigger names (left, right, up, down)
+    [SerializeField]
+    List<string> fishAnimations;
+
+    //added reference to animator
+    Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
         transform.position = fishSpawnLocation;
+        
+        //add animator component to the fish
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //destroys the fish if it goes out of bounds
+        if (transform.position.x < -10 || transform.position.x > 10 || transform.position.y < -10 || transform.position.y > 10)
+        {
+            //plays the spinning out animation
+            PlayRandomAnimation();
+        }
+
         transform.Translate(Vector2.right * -fishSpeed * Time.deltaTime);
     }
 
@@ -27,7 +44,22 @@ public class fishController : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+            PlayRandomAnimation();
             Destroy(gameObject);
         }
     }
+
+    private void PlayRandomAnimation()
+    {
+        if (fishAnimations.Count > 0)
+        {
+            //choose a random spin out animation from the list based on the trigger names
+            int randomAnimation = Random.Range(0, fishAnimations.Count);
+            string triggerName = fishAnimations[randomAnimation];
+
+            //play the animation based on the random trigger name
+            animator.SetTrigger(triggerName);
+        }
+    }
 }
+
