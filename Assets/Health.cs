0d117b0109ec7eed8bbe0bed7f330af79d315f64
21ttran11 <21ttran11@gmail.com>
2024-11-08 03:50:26 -0800

    health system

---
+++ Assets/Health.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> hearts;
+
+    private SpriteRenderer heartSprite;
+    // Start is called before the first frame update
+
+    int i = 0;
+
+    private bool alive = true;
+
+    [SerializeField]
+    private Sprite deadHeart;
+
+    private void Update()
+    {
+        if(i  == 3){
+            alive = false;
+            Debug.Log("game over");
+            Time.timeScale = 0f;
+        }
+    }
+    public void updateHeart()
+    {
+        GameObject heart = hearts[i];
+        heartSprite = heart.GetComponent<SpriteRenderer>();
+        heartSprite.sprite = deadHeart;
+        i++;
+    }
+}
--- Assets/Scripts/FishDestroyer.cs
+++ Assets/Scripts/FishDestroyer.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class FishDestroyer : MonoBehaviour
 {
@@ -8,12 +9,15 @@ public class FishDestroyer : MonoBehaviour
     public Animator lobsterAnimator;
     private GameObject localFish;
 
+    public UnityEvent attacked;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         lobsterAnimator.Play("Attacked");
         if (collision.gameObject.CompareTag("Fish"))
         {
             localFish = collision.gameObject;
+            attacked.Invoke();
         }
         Destroy(localFish);
     }
