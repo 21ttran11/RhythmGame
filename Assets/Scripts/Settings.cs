a96be0a799f2c2089dec63d1e2d0b986f0b6e3fa
Emily Tsai <emilytsai@csu.fullerton.edu>
2025-05-06 13:14:58 -0700

    Implemented fullscreen toggle and resolution dropdown for settings menu

--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -7,6 +7,8 @@ public class Settings : MonoBehaviour
 {
     [SerializeField]
     public Slider volumeSlider;
+    public TMPro.TMP_Dropdown resolution_dropdown;
+    Resolution[] resolutions;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +21,41 @@ public class Settings : MonoBehaviour
             Load();
             AudioListener.volume = volumeSlider.value;
         }
+
+        //for fullscreen toggle and resolution////////////////////////////////////////
+        //quality/////
+        int saved_quality = PlayerPrefs.GetInt("GraphicsQuality", QualitySettings.GetQualityLevel());
+        set_quality(saved_quality);
+
+        //fullscreen//////
+        bool saved_fullscreen = PlayerPrefs.GetInt("Fullscreen", 1) == 1;
+        set_fullscreen(saved_fullscreen);
+
+        //resolutions////////
+        resolutions = Screen.resolutions;
+        resolution_dropdown.ClearOptions();
+        //turn array of resolutions into formatetd strings
+        List<string> options = new List<string>();
+        //save
+        int saved_resolutionIndex = PlayerPrefs.GetInt("Resolution", 0);
+        set_resolution(saved_resolutionIndex);
+
+
+        int current_res = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                current_res = i;
+            }
+        }
+        resolution_dropdown.AddOptions(options);
+        resolution_dropdown.value = saved_resolutionIndex;
+        resolution_dropdown.RefreshShownValue();
     }
 
     public void ChangeVolume()
@@ -36,5 +73,25 @@ public class Settings : MonoBehaviour
     {
         PlayerPrefs.SetFloat("musicVolume", volumeSlider.value);
         PlayerPrefs.Save();
+    }
+    
+    //for fullscreen toggle and resolution////////////////////////////////////////
+    public void set_quality (int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+        PlayerPrefs.SetInt("GraphicsQuality", qualityIndex);
+    }
+
+    public void set_fullscreen (bool is_fullscreen)
+    {
+        Screen.fullScreen = is_fullscreen;
+        PlayerPrefs.SetInt("Fullscreen", is_fullscreen ? 1 : 0);
+    }
+
+    public void set_resolution (int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+        PlayerPrefs.SetInt("Resolution", resolutionIndex);
     }
 }
