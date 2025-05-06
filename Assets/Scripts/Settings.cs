7c9ad41b055a87e20f224e607ed542b219d80639
Emily Tsai <emilytsai@csu.fullerton.edu>
2025-05-06 10:10:49 -0700

    Moved assets + imported font

--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class VolumeSlider : MonoBehaviour
+public class Settings : MonoBehaviour
 {
     [SerializeField]
     public Slider volumeSlider;
